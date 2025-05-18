3dc6fb2244f7e73beb60bc184dd203a1c8d72ac1
Aryan <120791947+aryan25798@users.noreply.github.com>
2025-05-18 13:52:29 +0530

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -3,18 +3,17 @@ from typing import Literal, Optional
 from datetime import datetime
 
 class NotificationCreate(BaseModel):
-    id: str  # custom frontend id sent on creation
     user_id: int
     type: Literal["email", "sms", "in-app"]
     message: str
 
 class NotificationResponse(BaseModel):
-    id: str  # will hold custom_id from DB, not Mongo _id
+    id: str  # This maps to MongoDB's _id, returned as string
     user_id: int
     type: Literal["email", "sms", "in-app"]
     message: str
     status: str
-    created_at: Optional[datetime] = None  # Optional because it may not be always returned
+    created_at: Optional[datetime] = None
 
     class Config:
         allow_population_by_field_name = True
